e82065803a53b0ac8eaefe800d6d0a0ed6a0c5eb
Nathan de Souza <160131368+nathan-dza@users.noreply.github.com>
2026-04-23 15:48:38 -0700

    Started Story JS & css edits
    I think we might have made a mistake with the idea of our design, coding at least 50 screens for our thing will take a lot of time and im not sure if we can do that, we might have to rethink this

---
+++ story.js
@@ -0,0 +1,25 @@
+const story = [
+    {
+        id: 'intro',
+        scenes: [
+            {
+                speaker: 'Narrator',
+                text: "For centuries the luck goddess, Fortuna has looked upon this world. However in recent times, it seems that she has not been looking over you. Your mother has fallen sick and now you have to take care of her.",
+                img: ""//to be made
+            },
+
+            {   
+                img: "", //to be inserted
+                speaker: 'Player',
+                place: "home",
+                text: "Mann.... I need to get my mom some medecine from the town",
+                objective: 'Get mom medecine from the town',
+                choices: [{id: next, text: 'Go outside'}] //this choice only serves as a next
+            },
+
+            {
+                
+            }
+        ]
+    }
+]
